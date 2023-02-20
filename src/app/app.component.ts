28f57d0ffb96a2fdaa2949ed6b066927792e48d3
Aldo Daniel <60685239+aldodaniel01@users.noreply.github.com>
2023-02-20 17:45:56 -0600

    Creacion PipesApp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  nombre : string = 'Aldo daniel'
   title = 'pipesApp';
+
+  mostrarNombre() {
+    console.log(this.nombre);
+  }
 }
